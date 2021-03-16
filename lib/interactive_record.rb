339584ce8ea3a93f9d82e80a6fee7f18d8fa5436
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-16 01:47:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -119,11 +119,6 @@ class InteractiveRecord
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
-  end  
-        
-    
-  end  
+  end
 
-  
-  
 end  

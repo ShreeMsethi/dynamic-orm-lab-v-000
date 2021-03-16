333b24c0141628a7a6ef6959dfb34b597321c276
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-16 01:32:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -81,8 +81,11 @@ class InteractiveRecord
   def self.column_name
     DB[:conn].results_as_hash = true
     
-    sql = "PRAGMA table_info"
+    sql = "PRAGMA table_info('#{table_name}')"
+    
+    table_info = DB[:conn].execute(sql)
     column_name = []
+    
     
   end  
   

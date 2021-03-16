6f1b343959d5093a6ae374d315d3076a5cba907b
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-16 01:44:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -116,7 +116,7 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
-      values << self.send
+      values << "send
         
     
   end  
